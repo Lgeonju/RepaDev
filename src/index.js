811d711d217c5185b30c2f2ae71000cf55ce5949
이건주 <dlrjswn8747@nton.co.kr>
2024-07-23 11:26:50 +0900

    layout 제작중

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import NavArea from './fragment/NavArea';
+import Main from './fragment/Main';
 
 const root = ReactDOM.createRoot(document.getElementById('wrap'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavArea />
   </React.StrictMode>
 );
